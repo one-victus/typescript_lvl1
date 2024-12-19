e55bef8359efcf7a881e6c83bac1629fe19c70d1
one_victus <marvinjesus28@gmail.com>
2024-12-19 10:57:06 -0400

    add archivo demo del curso. Primera transpilación

---
+++ src/demo.ts
@@ -0,0 +1,40 @@
+(async ()=> {
+  interface Product {
+    price: number;
+  }
+  const myCart: Product[] = [];
+  const products: Product[] = [];
+  const limit = 2;
+
+  async function getProducts() {
+    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
+      method: 'GET'
+    });
+    const data = await rta.json();
+    products.concat(data);
+  }
+  function getTotal() {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+      total += products[i].price;
+    }
+    return total;
+  }
+  function addProduct(index) {
+    if (getTotal() <= limit) {
+      myCart.push(products[index]);
+    }
+  }
+
+  await getProducts();
+  addProduct(1);
+  addProduct(2);
+  const total = getTotal();
+  console.log(total);
+  const person = {
+    name: 'Nicolas',
+    lastName: 'Molina'
+  }
+  const rta = person['limit'] = limit;
+  console.log(rta);
+});
